3d3d8851226b049c4f4ae4edcab876eaa4d41874
Nikhil Parmar <nikhilparmarofc@gmail.com>
2025-12-14 11:42:20 +0530

    updated main program

---
+++ Part-1/Continue3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Continue3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Any Number: ");
+        int no = input.nextInt();
+
+        PrintEven(no);
+    }
+
+    public static void PrintEven(int no) {
+        for(int i=1; i<=no; i++) {
+            if(i % 2 == 1) {
+                continue;;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
